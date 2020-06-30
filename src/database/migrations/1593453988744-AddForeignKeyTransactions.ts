de65987f50b10f945d3eef842d0db66797e0093c
Heitor Gonzaga <heitorgonzaga@gmail.com>
2020-06-29 22:19:37 -0300

    .

---
+++ src/database/migrations/1593453988744-AddForeignKeyTransactions.ts
@@ -0,0 +1,35 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableForeignKey,
+  TableColumn,
+} from 'typeorm';
+
+export default class AddForeignKeyTransactions1593453988744
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'TranscationsCategories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TranscationsCategories');
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, useContainer, getRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: TransactionDTO): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+      if (total < value) {
+        throw new AppError('Saldo insuficiente para realizar a transação', 400);
+      }
+    }
+    let categoryTransaction = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+    if (!categoryTransaction) {
+      categoryTransaction = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(categoryTransaction);
+    }
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category: categoryTransaction,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
